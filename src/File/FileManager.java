bebd0c46d9f964c647fa449f579c31193f56e469
Jonatas <jonatas.silvestrini@gmail.com>
2018-10-24 13:27:49 -0300

    Criada a classe File Manager

---
+++ src/File/FileManager.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package File;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author JONATASWILLIAMSILVES
+ */
+public class FileManager {
+    
+    private File path;
+    private List<File> arquivos;
+    
+    public FileManager(File $path){
+        this.path = $path;
+    }
+    
+    public List ScanFiles(){
+        for (final File arquivo : this.path.listFiles()){
+            if (arquivo.getName().endsWith(".classe")){
+                arquivos.add(arquivo);
+            }
+        }
+        return arquivos;
+    }
+    
+}
